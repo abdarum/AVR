991c987b8cfbe20759c2c22c3cea8afb39f9943b
abdarum <36505427+abdarum@users.noreply.github.com>
2020-01-05 14:54:45 +0100

    Setup scripts for linux and whole repo

---
+++ Setup/dos2unix_whole_repo.sh
@@ -0,0 +1,2 @@
+sudo apt-get --yes install dos2unix
+find ../ -type f -print0 | xargs -0 -n 1 -P 4 dos2unix 
---
+++ Setup/setup_environment.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade all
+sudo apt-get --yes install gcc-avr binutils-avr avr-libc
+sudo apt-get --yes install gdb-avr
+sudo apt-get --yes install avrdude
+./dos2unix_whole_repo.sh
